2c4ab74aa04e089a9b0683d3376087fbde1dcd0b
Unknown <kaptainwutax@gmail.com>
2018-07-18 20:30:05 -0400

    1.0 Build

--- src/main/java/kaptainwutax/freecam/init/InitKeyBinding.java
+++ src/main/java/kaptainwutax/freecam/init/InitKeyBinding.java
@@ -7,7 +7,7 @@ import net.minecraftforge.fml.client.registry.ClientRegistry;
 
 public class InitKeyBinding {
 
-	public static KeyBinding ANCHOR = new KeyBinding("key.anchor.description", Keyboard.KEY_C, "key.freecam.category");
+	public static KeyBinding ANCHOR = new KeyBinding("key.anchor.description", Keyboard.KEY_Z, "key.freecam.category");
 	public static KeyBinding ROLL_L = new KeyBinding("key.roll_l.description", Keyboard.KEY_O, "key.freecam.category");
 	public static KeyBinding ROLL_R = new KeyBinding("key.roll_r.description", Keyboard.KEY_P, "key.freecam.category");
 	public static KeyBinding ROLL_RESET = new KeyBinding("key.roll_reset.description", Keyboard.KEY_I, "key.freecam.category");
